df45d2505378137f0a4387e1d4a01d752837fe4b
PacoSosax25 <pacososa025@gmail.com>
2026-04-21 12:06:22 -0600

    Se agrega clase principal java con println
    Pruebas de uso con jenkins con git

---
+++ Principal.java
@@ -0,0 +1,9 @@
+public class Principal { 
+    public static void main(String[] args) { 
+        
+            System.out.println("Este es un ejemplo desde GIT en la clase principal"); 
+        
+    } 
+ 
+  
+}
