be3dbd75d0c35ede017ef617508a547636c2b7ea
PacoSosax25 <pacososa025@gmail.com>
2026-04-22 12:38:49 -0600

    Update output message in Principal class

--- Principal.java
+++ Principal.java
@@ -1,7 +1,7 @@
 public class Principal { 
     public static void main(String[] args) { 
         
-            System.out.println("Este es un ejemplo desde GIT en la clase principal"); 
+            System.out.println("Este es un ejemplo desde GIT en la clase principalAAAAAAAAAAAAA"); 
         
     } 
  
